d9dd7a0e52ba2faff6fe9542d981a18a16e5d7ba
Takashi Kanemoto <ttskch@gmail.com>
2022-05-27 01:21:49 +0900

    fix: 🐛 image building

--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -8,6 +8,7 @@ LATEST_TAG=`echo $TAGS | sed -r "s/^(.* )?(.+)$/\2/"`
 for TAG in $TAGS; do
   PHP_VERSION=`echo $TAG | sed -r "s/php-(.+)\/deployer-(.+)/\1/"`
   DEPLOYER_VERSION=`echo $TAG | sed -r "s/php-(.+)\/deployer-(.+)/\2/"`
+  IMAGE_TAG=$DEPLOYER_VERSION
 
   if [[ $DEPLOYER_VERSION =~ ^[0-9]+(\.[0-9]+)?$ ]] ; then
     DEPLOYER_VERSION=$DEPLOYER_VERSION.*
@@ -16,14 +17,14 @@ for TAG in $TAGS; do
   docker build \
     --build-arg PHP_VERSION=$PHP_VERSION \
     --build-arg DEPLOYER_VERSION=$DEPLOYER_VERSION \
-    -t ttskch/deployer:$DEPLOYER_VERSION \
+    -t ttskch/deployer:$IMAGE_TAG \
     .
-  docker push ttskch/deployer:$DEPLOYER_VERSION
+  docker push ttskch/deployer:$IMAGE_TAG
 
   if [ $TAG = $LATEST_TAG ]; then
-    docker tag ttskch/deployer:$DEPLOYER_VERSION ttskch/deployer:latest
+    docker tag ttskch/deployer:$IMAGE_TAG ttskch/deployer:latest
     docker push ttskch/deployer:latest
   fi
 
-  docker rmi ttskch/deployer:$DEPLOYER_VERSION
+  docker rmi ttskch/deployer:$IMAGE_TAG
 done
