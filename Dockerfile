6951f19d17b80580899c167f871e2d5eace0c40f
Takashi Kanemoto <ttskch@gmail.com>
2022-05-27 11:57:52 +0900

    fix: 🐛 use composer:2.2 instead of composer:latest on php <7.2.5
    https://github.com/composer/composer/releases/tag/2.3.0
    https://github.com/docker/for-mac/issues/2155#issuecomment-338619432

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,12 @@
 ARG PHP_VERSION
+ARG COMPOSER_VERSION
+FROM composer:$COMPOSER_VERSION as COMPOSER
+
 FROM php:$PHP_VERSION-cli-alpine
 
-COPY --from=composer:latest /usr/bin/composer /usr/bin/composer
+COPY --from=COMPOSER /usr/bin/composer /usr/bin/composer
 
 ARG DEPLOYER_VERSION
-
 RUN \
   composer global require deployer/dist:$DEPLOYER_VERSION \
   && ln -sf ~/.composer/vendor/bin/dep /usr/bin/dep \
--- bin/rebuild.sh
+++ bin/rebuild.sh
@@ -10,15 +10,23 @@ for TAG in $TAGS; do
   DEPLOYER_VERSION=`echo $TAG | sed -r "s/php-(.+)\/deployer-(.+)/\2/"`
   IMAGE_TAG=$DEPLOYER_VERSION
 
+  if [ $PHP_VERSION = '5' ] || [ $PHP_VERSION = '7.0' ]; then
+    COMPOSER_VERSION=2.2
+  else
+    COMPOSER_VERSION=latest
+  fi
+
   if [[ $DEPLOYER_VERSION =~ ^[0-9]+(\.[0-9]+)?$ ]] ; then
     DEPLOYER_VERSION=$DEPLOYER_VERSION.*
   fi
 
   docker build \
     --build-arg PHP_VERSION=$PHP_VERSION \
+    --build-arg COMPOSER_VERSION=$COMPOSER_VERSION \
     --build-arg DEPLOYER_VERSION=$DEPLOYER_VERSION \
     -t ttskch/deployer:$IMAGE_TAG \
     .
+
   docker push ttskch/deployer:$IMAGE_TAG
 
   if [ $TAG = $LATEST_TAG ]; then
